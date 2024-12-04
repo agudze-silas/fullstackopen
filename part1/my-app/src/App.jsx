4d43f0f6601a51eab3cc9033b4597707d4c0108b
agudze-silas <agudzesilas@gmail.com>
2024-12-04 23:44:49 +0000

    refactored code

--- part1/my-app/src/App.jsx
+++ part1/my-app/src/App.jsx
@@ -14,7 +14,7 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) =>{
+const Total = (props) => {
   return (
     <div>
       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
@@ -24,33 +24,35 @@ const Total = (props) =>{
 
 const Part = (props) => {
   return (
-  <p>
-    {props.part} {props.exercises}
-  </p>
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
